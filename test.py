6b0a0668c539c667b9f6416fa2283ec617c184e7
David You(shyou) <shyou@cisco.com>
2024-12-10 17:17:52 +0800

    Support akshare

--- test.py
+++ test.py
@@ -1,58 +1,76 @@
+from datetime import datetime, timedelta
+import akshare as ak
+from operator import itemgetter
+
 import matplotlib.pyplot as plt
 import numpy as np
 import yfinance as yf
+from akshare import volatility_yz_rv
 from arch import arch_model
 
-#ticker = '516080.SS'
 tickers = [
-#    {'ticker': '159851.SZ', 'name': '金融科技'},
-#    {'ticker': '588290.SS', 'name': '科创芯片'},
-    {'ticker': '516770.SS', 'name': '动漫游戏'},
-#    {'ticker': '159899.SZ', 'name': '软件指数'},
-#    {'ticker': '588680.SS', 'name': '科创100'},
-#    {'ticker': '515880.SS', 'name': '通信设备'},
-#    {'ticker': '161628.SZ', 'name': '云计算'},
-#    {'ticker': '159755.SZ', 'name': 'CS电池'},
-    {'ticker': '159847.SZ', 'name': '中证医疗'},
+    {'ticker': '159851.SZ', 'name': '金融科技'},  #0.6
+    {'ticker': '588290.SS', 'name': '科创芯片'}, #0.6
+    {'ticker': '516770.SS', 'name': '动漫游戏'}, #0.5
+    {'ticker': '159899.SZ', 'name': '软件指数'},  #0.4~0.5
+    {'ticker': '515880.SS', 'name': '通信设备'},  #0.4
+    {'ticker': '516510.SS', 'name': '云计算'},    #0.5
+    {'ticker': '159847.SZ', 'name': '中证医疗'},  #0.3
     {'ticker': '510300.SS', 'name': '沪深300'},
-#    {'ticker': '510050.SS', 'name': '上证50'},
-#    {'ticker': '513010.SS', 'name': '恒生科技'},
-    {'ticker': '512660.SS', 'name': '中证军工'},
-#    {'ticker': '159805.SZ', 'name': '中证传媒'},
-#    {'ticker': '159628.SZ', 'name': '国证2000'},
-#    {'ticker': '159928.SZ', 'name': '中证消费'},
-#    {'ticker': '159857.SZ', 'name': '光伏产业'},
-    {'ticker': '159915.SZ', 'name': '创业板'},
+    {'ticker': '510050.SS', 'name': '上证50'},    #0.2~0.3
+    {'ticker': '513130.SS', 'name': '恒生科技'},  #0.3
+    {'ticker': '512660.SS', 'name': '中证军工'},  #0.4~0.5
+    {'ticker': '159805.SZ', 'name': '中证传媒'},   #0.4~0.5
+    {'ticker': '159628.SZ', 'name': '国证2000'},
+    {'ticker': '159928.SZ', 'name': '中证消费'},  #0.3
+    {'ticker': '159857.SZ', 'name': '光伏产业'}, #0.4~0.5
+    {'ticker': '159948.SZ', 'name': '创业板'},   #0.3
+    {'ticker': '588210.SS', 'name': '科创100'},  #0.5
+    {'ticker': '159667.SZ', 'name': '中证机床'},  #0.3~0.4
+    {'ticker': '588830.SS', 'name': '科创新能源'}, #0.5~0.6
+    {'ticker': '588680.SS', 'name': '科创医药'},  #0.5
+    {'ticker': '513280.SS', 'name': '港股医药'},  #0.3
+    {'ticker': '515700.SS', 'name': '新能车'},    #0.4
+    {'ticker': '588010.SS', 'name': '科创材料'}, #0.5
+    {'ticker': '159855.SZ', 'name': '中证影视'},  #0.4
+    {'ticker': '512400.SS', 'name': '有色金属'},  #0.3
+    {'ticker': '512710.SS', 'name': '军工龙头'},  #0.4
+    {'ticker': '159813.SZ', 'name': '国证芯片'},
+    {'ticker': '163406.SZ', 'name': 'xqhr'},
+    {'ticker': '600438.SS', 'name': 'twgf'},
+    {'ticker': '300274.SZ', 'name': 'ygdy'},
 ]
 
+def getData2(t):
+    stock_zh_a_spot_df = ak.stock_zh_a_minute(symbol=t, period='15', adjust='qfq')
+    print(stock_zh_a_spot_df.tail(5))
+    stock_zh_a_spot_df['Return'] = stock_zh_a_spot_df['close'].astype(float).pct_change()
+    volatility = np.std(stock_zh_a_spot_df['Return'].dropna())
+    annual_volatility = volatility * np.sqrt(252 * 6.5 * 4)
+    print(annual_volatility)
+    return annual_volatility
 
 def getData(t):
-    data = yf.download(t, start='2018-12-01', end='2024-12-28', interval='1d')
-    print(data.head())
+    current = datetime.now() + timedelta(days=1)
+    last_30_day = current - timedelta(days=7)
+
+    data = yf.download(t, start=last_30_day.strftime('%Y-%m-%d'), end=current.strftime('%Y-%m-%d'), interval='15m')
+    #print(data.tail(5))
+    print(data['Adj Close'])
 
     data['Return'] = data['Adj Close'].pct_change()
-    daily_volatility = np.std(data['Return'].dropna())
-    annual_volatility = daily_volatility * np.sqrt(252)
-    print(f"Annual Volatility: {annual_volatility}")
+    volatility = np.std(data['Return'].dropna())
+    annual_volatility = volatility * np.sqrt(252 * 6.5 * 4)
+#    print(f" volatility: {annual_volatility}")
+    return annual_volatility
 
-    window_size = 30  # 30天滚动窗口
-    return data['Return'].rolling(window=window_size).std() * np.sqrt(252)
 
-plt.rcParams['font.family'] = 'Heiti TC'
-plt.figure(figsize=(10, 6))
 for tt in tickers:
-    plt.plot(getData(tt.get('ticker')), label=tt.get('name'))
-plt.title('Rolling Volatility Over Time')
-plt.xlabel('Date')
-plt.ylabel('Volatility')
-plt.legend()
-plt.show()
+#    tt['volatility'] = getData(tt.get('ticker'))
+    tt['volatility'] = getData2(tt.get('ticker'))
 
+sorted_dict = sorted(tickers, key=lambda x : x['volatility'])
 
-
-
-
-#model = arch_model(data['Return'].dropna(), vol='Garch', p=1, q=1)
-#model_fit = model.fit(disp='off')
-#print(model_fit.summary())
-
+# 输出排序后的字典
+for tt in sorted_dict:
+    print(tt)
